88ef93413826a27574131876565db3e974dcd745
Agustin Espinoza <agustinurdz_@hotmail.com>
2016-08-20 17:57:34 -0500

    Initial commit, includes the first project version.

--- AgusRdz_TFS_TestCaseExportTool/Form1.Designer.cs
+++ AgusRdz_TFS_TestCaseExportTool/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace AgusRdz_TFS_TestCaseExportTool
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,313 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.gbExport = new System.Windows.Forms.GroupBox();
+            this.NoSubSuite = new System.Windows.Forms.CheckBox();
+            this.ExportResults = new System.Windows.Forms.CheckBox();
+            this.SeparateSheets = new System.Windows.Forms.CheckBox();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.gbTarget = new System.Windows.Forms.GroupBox();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.btnFolderBrowse = new System.Windows.Forms.Button();
+            this.txtSaveFolder = new System.Windows.Forms.TextBox();
+            this.lblDestination = new System.Windows.Forms.Label();
+            this.gbSource = new System.Windows.Forms.GroupBox();
+            this.comBoxTestPlan = new System.Windows.Forms.ComboBox();
+            this.lblTestSuite = new System.Windows.Forms.Label();
+            this.treeView_suite = new System.Windows.Forms.TreeView();
+            this.lblTestPlan = new System.Windows.Forms.Label();
+            this.btnTeamProject = new System.Windows.Forms.Button();
+            this.txtTeamProject = new System.Windows.Forms.TextBox();
+            this.lblTeamProject = new System.Windows.Forms.Label();
+            this.gbExport.SuspendLayout();
+            this.gbTarget.SuspendLayout();
+            this.gbSource.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbExport
+            // 
+            this.gbExport.Controls.Add(this.NoSubSuite);
+            this.gbExport.Controls.Add(this.ExportResults);
+            this.gbExport.Controls.Add(this.SeparateSheets);
+            this.gbExport.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbExport.Location = new System.Drawing.Point(274, 171);
+            this.gbExport.Name = "gbExport";
+            this.gbExport.Size = new System.Drawing.Size(378, 174);
+            this.gbExport.TabIndex = 19;
+            this.gbExport.TabStop = false;
+            this.gbExport.Text = "Opciones de exportación:";
+            // 
+            // NoSubSuite
+            // 
+            this.NoSubSuite.AutoSize = true;
+            this.NoSubSuite.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NoSubSuite.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.NoSubSuite.Location = new System.Drawing.Point(11, 40);
+            this.NoSubSuite.Name = "NoSubSuite";
+            this.NoSubSuite.Size = new System.Drawing.Size(315, 34);
+            this.NoSubSuite.TabIndex = 14;
+            this.NoSubSuite.Text = "Export the selected suite.\r\n(The test cases of the sub suites will not be exporte" +
+    "d)";
+            this.NoSubSuite.UseVisualStyleBackColor = true;
+            // 
+            // ExportResults
+            // 
+            this.ExportResults.AutoSize = true;
+            this.ExportResults.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ExportResults.Location = new System.Drawing.Point(11, 137);
+            this.ExportResults.Name = "ExportResults";
+            this.ExportResults.Size = new System.Drawing.Size(134, 19);
+            this.ExportResults.TabIndex = 13;
+            this.ExportResults.Text = "Export tests results.";
+            this.ExportResults.UseVisualStyleBackColor = true;
+            // 
+            // SeparateSheets
+            // 
+            this.SeparateSheets.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SeparateSheets.Location = new System.Drawing.Point(11, 89);
+            this.SeparateSheets.Name = "SeparateSheets";
+            this.SeparateSheets.Size = new System.Drawing.Size(323, 42);
+            this.SeparateSheets.TabIndex = 0;
+            this.SeparateSheets.Text = "Export each set of tests on separate sheets.";
+            this.SeparateSheets.UseVisualStyleBackColor = true;
+            // 
+            // btnHelp
+            // 
+            this.btnHelp.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHelp.Location = new System.Drawing.Point(274, 365);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(88, 25);
+            this.btnHelp.TabIndex = 20;
+            this.btnHelp.Text = "About...";
+            this.btnHelp.UseVisualStyleBackColor = true;
+            // 
+            // btnExport
+            // 
+            this.btnExport.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExport.Location = new System.Drawing.Point(469, 365);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(82, 25);
+            this.btnExport.TabIndex = 17;
+            this.btnExport.Text = "Start";
+            this.btnExport.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Location = new System.Drawing.Point(570, 365);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(82, 25);
+            this.btnCancel.TabIndex = 18;
+            this.btnCancel.Text = "Exit";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // gbTarget
+            // 
+            this.gbTarget.Controls.Add(this.txtFileName);
+            this.gbTarget.Controls.Add(this.lblFileName);
+            this.gbTarget.Controls.Add(this.btnFolderBrowse);
+            this.gbTarget.Controls.Add(this.txtSaveFolder);
+            this.gbTarget.Controls.Add(this.lblDestination);
+            this.gbTarget.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbTarget.Location = new System.Drawing.Point(274, 21);
+            this.gbTarget.Name = "gbTarget";
+            this.gbTarget.Size = new System.Drawing.Size(378, 144);
+            this.gbTarget.TabIndex = 15;
+            this.gbTarget.TabStop = false;
+            this.gbTarget.Text = "Target:";
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.BackColor = System.Drawing.Color.White;
+            this.txtFileName.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFileName.Location = new System.Drawing.Point(11, 98);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(323, 24);
+            this.txtFileName.TabIndex = 5;
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFileName.Location = new System.Drawing.Point(8, 75);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(115, 15);
+            this.lblFileName.TabIndex = 0;
+            this.lblFileName.Text = "Exported File Name:";
+            // 
+            // btnFolderBrowse
+            // 
+            this.btnFolderBrowse.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFolderBrowse.Location = new System.Drawing.Point(253, 40);
+            this.btnFolderBrowse.Name = "btnFolderBrowse";
+            this.btnFolderBrowse.Size = new System.Drawing.Size(81, 28);
+            this.btnFolderBrowse.TabIndex = 4;
+            this.btnFolderBrowse.Text = "Browse...";
+            this.btnFolderBrowse.UseVisualStyleBackColor = true;
+            // 
+            // txtSaveFolder
+            // 
+            this.txtSaveFolder.BackColor = System.Drawing.Color.White;
+            this.txtSaveFolder.Enabled = false;
+            this.txtSaveFolder.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSaveFolder.ForeColor = System.Drawing.Color.Black;
+            this.txtSaveFolder.Location = new System.Drawing.Point(11, 42);
+            this.txtSaveFolder.Name = "txtSaveFolder";
+            this.txtSaveFolder.ReadOnly = true;
+            this.txtSaveFolder.Size = new System.Drawing.Size(236, 24);
+            this.txtSaveFolder.TabIndex = 7;
+            // 
+            // lblDestination
+            // 
+            this.lblDestination.AutoSize = true;
+            this.lblDestination.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDestination.Location = new System.Drawing.Point(8, 19);
+            this.lblDestination.Name = "lblDestination";
+            this.lblDestination.Size = new System.Drawing.Size(130, 15);
+            this.lblDestination.TabIndex = 0;
+            this.lblDestination.Text = "File location to export:";
+            // 
+            // gbSource
+            // 
+            this.gbSource.Controls.Add(this.comBoxTestPlan);
+            this.gbSource.Controls.Add(this.lblTestSuite);
+            this.gbSource.Controls.Add(this.treeView_suite);
+            this.gbSource.Controls.Add(this.lblTestPlan);
+            this.gbSource.Controls.Add(this.btnTeamProject);
+            this.gbSource.Controls.Add(this.txtTeamProject);
+            this.gbSource.Controls.Add(this.lblTeamProject);
+            this.gbSource.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbSource.Location = new System.Drawing.Point(24, 21);
+            this.gbSource.Name = "gbSource";
+            this.gbSource.Size = new System.Drawing.Size(244, 369);
+            this.gbSource.TabIndex = 16;
+            this.gbSource.TabStop = false;
+            this.gbSource.Text = "Origen";
+            this.gbSource.Enter += new System.EventHandler(this.gbSource_Enter);
+            // 
+            // comBoxTestPlan
+            // 
+            this.comBoxTestPlan.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comBoxTestPlan.DropDownWidth = 200;
+            this.comBoxTestPlan.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comBoxTestPlan.FormattingEnabled = true;
+            this.comBoxTestPlan.Location = new System.Drawing.Point(11, 98);
+            this.comBoxTestPlan.Name = "comBoxTestPlan";
+            this.comBoxTestPlan.Size = new System.Drawing.Size(215, 23);
+            this.comBoxTestPlan.TabIndex = 2;
+            // 
+            // lblTestSuite
+            // 
+            this.lblTestSuite.AutoSize = true;
+            this.lblTestSuite.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTestSuite.Location = new System.Drawing.Point(8, 139);
+            this.lblTestSuite.Name = "lblTestSuite";
+            this.lblTestSuite.Size = new System.Drawing.Size(61, 15);
+            this.lblTestSuite.TabIndex = 12;
+            this.lblTestSuite.Text = "Test Suite:";
+            // 
+            // treeView_suite
+            // 
+            this.treeView_suite.AllowDrop = true;
+            this.treeView_suite.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.treeView_suite.HideSelection = false;
+            this.treeView_suite.Location = new System.Drawing.Point(11, 160);
+            this.treeView_suite.Name = "treeView_suite";
+            this.treeView_suite.Size = new System.Drawing.Size(217, 200);
+            this.treeView_suite.TabIndex = 9;
+            // 
+            // lblTestPlan
+            // 
+            this.lblTestPlan.AutoSize = true;
+            this.lblTestPlan.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTestPlan.Location = new System.Drawing.Point(8, 75);
+            this.lblTestPlan.Name = "lblTestPlan";
+            this.lblTestPlan.Size = new System.Drawing.Size(59, 15);
+            this.lblTestPlan.TabIndex = 0;
+            this.lblTestPlan.Text = "Test Plan:";
+            // 
+            // btnTeamProject
+            // 
+            this.btnTeamProject.Location = new System.Drawing.Point(191, 43);
+            this.btnTeamProject.Name = "btnTeamProject";
+            this.btnTeamProject.Size = new System.Drawing.Size(35, 23);
+            this.btnTeamProject.TabIndex = 1;
+            this.btnTeamProject.Text = "...";
+            this.btnTeamProject.UseVisualStyleBackColor = true;
+            // 
+            // txtTeamProject
+            // 
+            this.txtTeamProject.BackColor = System.Drawing.Color.White;
+            this.txtTeamProject.Enabled = false;
+            this.txtTeamProject.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTeamProject.ForeColor = System.Drawing.Color.Black;
+            this.txtTeamProject.Location = new System.Drawing.Point(11, 42);
+            this.txtTeamProject.Name = "txtTeamProject";
+            this.txtTeamProject.ReadOnly = true;
+            this.txtTeamProject.Size = new System.Drawing.Size(172, 24);
+            this.txtTeamProject.TabIndex = 0;
+            // 
+            // lblTeamProject
+            // 
+            this.lblTeamProject.AutoSize = true;
+            this.lblTeamProject.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTeamProject.Location = new System.Drawing.Point(8, 21);
+            this.lblTeamProject.Name = "lblTeamProject";
+            this.lblTeamProject.Size = new System.Drawing.Size(147, 15);
+            this.lblTeamProject.TabIndex = 0;
+            this.lblTeamProject.Text = "Visual Studio Connection:";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(676, 411);
+            this.Controls.Add(this.gbExport);
+            this.Controls.Add(this.btnHelp);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.gbTarget);
+            this.Controls.Add(this.gbSource);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TFS Test Case Export Tool";
+            this.gbExport.ResumeLayout(false);
+            this.gbExport.PerformLayout();
+            this.gbTarget.ResumeLayout(false);
+            this.gbTarget.PerformLayout();
+            this.gbSource.ResumeLayout(false);
+            this.gbSource.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox gbExport;
+        private System.Windows.Forms.CheckBox NoSubSuite;
+        private System.Windows.Forms.CheckBox ExportResults;
+        private System.Windows.Forms.CheckBox SeparateSheets;
+        private System.Windows.Forms.Button btnHelp;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.GroupBox gbTarget;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Label lblFileName;
+        private System.Windows.Forms.Button btnFolderBrowse;
+        private System.Windows.Forms.TextBox txtSaveFolder;
+        private System.Windows.Forms.Label lblDestination;
+        private System.Windows.Forms.GroupBox gbSource;
+        private System.Windows.Forms.ComboBox comBoxTestPlan;
+        private System.Windows.Forms.Label lblTestSuite;
+        private System.Windows.Forms.TreeView treeView_suite;
+        private System.Windows.Forms.Label lblTestPlan;
+        private System.Windows.Forms.Button btnTeamProject;
+        private System.Windows.Forms.TextBox txtTeamProject;
+        private System.Windows.Forms.Label lblTeamProject;
     }
 }
 
--- AgusRdz_TFS_TestCaseExportTool/Form1.cs
+++ AgusRdz_TFS_TestCaseExportTool/Form1.cs
@@ -10,11 +10,150 @@ using System.Windows.Forms;
 
 namespace AgusRdz_TFS_TestCaseExportTool
 {
-    public partial class Form1 : Form
+    public partial class frmMain : Form
     {
-        public Form1()
+        // Detectar una colección de proyectos.
+        // Proporcionar las credenciales del usuario.
+        private TfsTeamProjectCollection _tfs;
+        private ITestManagementTeamProject _teamProject;
+        private ITestPlanCollection plans;
+        private ITestCaseCollection testCases;
+        private WorkItemStore _store = null;
+        Excel.Application xlApp;
+        Excel.Workbook xlWorkBook;
+        Excel.Range chartRange;
+        object misValue = System.Reflection.Missing.Value;
+        int row = 2;
+        int upperBound = 0;
+        int lowerBound = 0;
+        int sheetNum = 1;
+        int defaultSheets;
+
+        private delegate void Execute();
+
+        public frmMain()
         {
             InitializeComponent();
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Margins
+        {
+            public int derecha, izquierda, superior, inferior;
+        }
+        [DllImport("dwmapi.dll")]
+        public static extern int DwmExtendFrameIntoClientArea(IntPtr hWnd, ref Margins margs);
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+            BackColor = System.Drawing.Color.Gray;
+            Margins margenes = new Margins();
+            margenes.derecha = -1;
+            margenes.izquierda = -1;
+            margenes.superior = -1;
+            margenes.inferior = -1;
+            IntPtr hwn = this.Handle;
+            //int result = DwmExtendFrameIntoClientArea(hWnd, ref margenes);
+
+        }
+
+        /// <summary>
+        /// Permite seleccionar el proyacto de Visual Studio al que se quiere conectar
+        /// para realizar la exportación
+        /// </summary>
+        private void btnTeamProject_Click(object sender, EventArgs e)
+        {
+            // Muestra un cuadro de dialogo con los proyectos disponibles
+            TeamProjectPicker tpp = new TeamProjectPicker(TeamProjectPickerMode.SingleProject, false);
+            tpp.ShowDialog();
+
+            // Si se selecciona un proyecto
+            if (tpp.SelectedTeamProjectCollection != null)
+            {
+                this._tfs = tpp.SelectedTeamProjectCollection;
+                ITestManagementService test_service = (ITestManagementService)_tfs.GetService(typeof(ITestManagementService));
+                _store = (WorkItemStore)_tfs.GetService(typeof(WorkItemStore));
+                this._teamProject = test_service.GetTeamProject(tpp.SelectedProjects[0].Name);
+
+                // Mostrar el nombre del proyecto seleccionado
+                txtTeamProject.Text = tpp.SelectedProjects[0].Name;
+                // Recuperar los planes de prueba existentes
+                Get_TestPlans(_teamProject);
+            }
+        }
+
+        /// <sumary>    
+        /// Recupera los planes de prueba que estan contenidos en el proyecto
+        /// </sumary>
+        private void Get_TestPlans(ITestManagementTeamProject teamProject)
+        {
+            // Consultar los planes
+            this.plans = teamProject.TestPlans.Query("SELECT * FROM TestPlan");
+            // Mostrar los planes en el combobox
+            comBoxTestPlan.Items.Clear();
+            // Llenar el árbol con las suites
+            treeView_suite.BackColor = Color.White;
+            foreach (ITestPlan plan in plans)
+            {
+                comBoxTestPlan.Items.Add(plan.Name);
+            }
+        }
+
+        /// <summary>
+        /// Mostrar las suites contenidas en los planes de prueba
+        /// </summary>
+        private void comBoxTestPlan_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.WaitCursor;
+            treeView_suite.Nodes.Clear();
+            int i = -1;
+            if (comBoxTestPlan.SelectedIndex >= 0)
+            {
+                i = comBoxTestPlan.SelectedIndex;
+                this.Cursor = Cursors.Arrow;
+                if (plans[i].RootSuite != null)
+                {
+                    TreeNode rootNode = new TreeNode();
+                    rootNode.Name = plans[i].RootSuite.Id.ToString();
+                    rootNode.Text = plans[i].RootSuite.Title.ToString();
+                    treeView_suite.Nodes.Add(rootNode);
+                    if (plans[i].RootSuite.SubSuites != null && plans[i].RootSuite.SubSuites.Count > 0)
+                    {
+                        Get_subsuites(plans[i].RootSuite, rootNode);
+                    }
+                }
+            }
+        }
+
+        /// <sumary>
+        /// Muestra las subsuites contenidas en la suite primaria
+        /// </sumary>
+        private void Get_subsuites(IStaticTestSuite rootsuite1, TreeNode node1)
+        {
+            ITestSuiteCollection subsuite1 = rootsuite1.SubSuites;
+            foreach (ITestSuiteBase suite in subsuite1)
+            {
+                if (suite != null)
+                {
+                    TreeNode subnode = new TreeNode();
+                    subnode.Text = suite.Title.ToString();
+                    subnode.Name = suite.Id.ToString();
+                    node1.Nodes.Add(subnode);
+                    if (suite.TestSuiteType == TestSuiteType.StaticTestSuite)
+                    {
+                        IStaticTestSuite subsuite2 = suite as IStaticTestSuite;
+                        if (subsuite2 != null && (subsuite2.SubSuites.Count > 0))
+                        {
+                            Get_subsuites(subsuite2, subnode);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
